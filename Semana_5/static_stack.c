18ad8f28995c651ee0defd4669cba03fd491ddde
thiagooruby <trs3@aluno.ifal.edu.br>
2023-02-14 20:54:06 -0300

    week 5

---
+++ Semana_5/static_stack.c
@@ -0,0 +1,30 @@
+// Static stack
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXN 500
+
+typedef struct snode{
+    int value;
+} SNode;
+
+// create
+// push
+// pop
+// len
+// sprint
+// top
+// empty
+
+// sort
+// union
+// invert
+
+int main()
+{
+    // initialize stack
+    SNode stack = malloc(sizeof(SNode) * MAXN);
+    int top;
+    
+    return 0;
+}
